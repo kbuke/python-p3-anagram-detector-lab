be9cb5584bdf58e332e98d72546802c3914ef5b1
kaan <kabuke13@gmail.com>
2024-01-29 14:50:25 +0000

    lab changes

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,22 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+    
+    @property
+    def word(self):
+        return self._word 
+    
+    @word.setter
+    def word(self, word):
+        if isinstance(word, str):
+            self._word = word
+        else:
+            print("word needs to be a string")
+
+        
+    
+    def match(self, word_list):
+        return [word for word in word_list if self.is_anagram(word)]
+    
+    def is_anagram(self, word):
+        return sorted(self.word.lower()) == sorted(word.lower())
